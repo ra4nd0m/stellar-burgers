9c7ef6c43675417db30df078613f9d7ec7feaf3f
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2025-03-28 19:55:16 +0300

    feat: fetch user and ingredients data on AppBody mount

--- src/components/app-body/app-body.tsx
+++ src/components/app-body/app-body.tsx
@@ -13,71 +13,83 @@ import { Route, Routes } from 'react-router-dom';
 import { OrderInfo } from '../order-info';
 import { IngredientDetails } from '../ingredient-details';
 import { ProtectedRoute } from '../protected-route';
+import { useEffect } from 'react';
+import { getUser } from './../../services/slices/user';
+import { getIngredients } from './../../services/slices/ingredients';
+import { useDispatch } from './../../services/store';
 
-export const AppBody = () => (
-  <>
-    <Routes>
-      <Route path='/' element={<ConstructorPage />} />
-      <Route path='/feed' element={<Feed />} />
-      <Route path='/feed/:number' element={<OrderInfo />} />
-      <Route path='/ingredients/:id' element={<IngredientDetails />} />
-      <Route
-        path='/login'
-        element={
-          <ProtectedRoute onlyUnAuth>
-            <Login />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/register'
-        element={
-          <ProtectedRoute onlyUnAuth>
-            <Register />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/forgot-password'
-        element={
-          <ProtectedRoute onlyUnAuth>
-            <ForgotPassword />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/reset-password'
-        element={
-          <ProtectedRoute onlyUnAuth>
-            <ResetPassword />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/profile'
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/profile/orders'
-        element={
-          <ProtectedRoute>
-            <ProfileOrders />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/profile/orders/:number'
-        element={
-          <ProtectedRoute>
-            <OrderInfo />
-          </ProtectedRoute>
-        }
-      />
-      <Route path='*' element={<NotFound404 />} />
-    </Routes>
-  </>
-);
+export const AppBody = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(getIngredients());
+  }, [dispatch]);
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <ProtectedRoute>
+              <OrderInfo />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </>
+  );
+};
