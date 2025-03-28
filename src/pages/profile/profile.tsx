2a821c1069e1556034843b69a527385bc5a82701
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2025-03-29 00:57:20 +0300

    feat: integrate Redux for user profile management and update functionality

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,12 +1,12 @@
 import { ProfileUI } from '@ui-pages';
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getUserSelector, updateUser } from './../../services/slices/user';
+import { useDispatch } from './../../services/store';
 
 export const Profile: FC = () => {
-  /** TODO: взять переменную из стора */
-  const user = {
-    name: '',
-    email: ''
-  };
+  const dispatch = useDispatch();
+  const user = useSelector(getUserSelector);
 
   const [formValue, setFormValue] = useState({
     name: user.name,
@@ -29,6 +29,7 @@ export const Profile: FC = () => {
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(updateUser(formValue));
   };
 
   const handleCancel = (e: SyntheticEvent) => {
